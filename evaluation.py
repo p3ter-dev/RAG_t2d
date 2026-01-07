49c7547ed27845201f06ec48f9979a4b88528728
peter <peterkinfe548@gmail.com>
2026-01-07 22:53:15 +0300

    evaluation file

--- evaluation.py
+++ evaluation.py
@@ -20,7 +20,7 @@ vectorstore = FAISS.load_local(
     allow_dangerous_deserialization=True
 )
 
-retriever = vectorstore.as_retriever(search_kwargs={"k": 4})
+retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
 
 # LLM
 llm = ChatGoogleGenerativeAI(
